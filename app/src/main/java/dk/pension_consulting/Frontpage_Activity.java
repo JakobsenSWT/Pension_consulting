69e3a1ca83ecdfa9f93721796d24f4aebe1a6e1d
jonathanlarsen <jal@atol.dk>
2018-01-03 09:50:53 +0100

    Something, nothing

--- app/src/main/java/dk/pension_consulting/Frontpage_Activity.java
+++ app/src/main/java/dk/pension_consulting/Frontpage_Activity.java
@@ -3,11 +3,17 @@ package dk.pension_consulting;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+
 public class Frontpage_Activity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_frontpage_);
+
+
+
     }
+
+
 }
